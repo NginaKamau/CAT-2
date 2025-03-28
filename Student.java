6363a10623976c51b24b8926718ae5272c85a1f4
NginaKamau <loisek.ngina@strathmore.edu>
2025-03-28 19:17:54 +0300

    Creation of the method that deals with the grades and the method to display the details

--- Student.java
+++ Student.java
@@ -6,7 +6,7 @@ public class Student {
     //creating the constructor that will be used for the getter and setter
     public Student(String name,int grade){
         this.name=name;
-        setGrade(grade);
+        setGrade(grade);//this is the setter for grade which will ensure grading within 0 and 100
     }
     //getter for the name
     public String getName(){
@@ -21,5 +21,21 @@ public class Student {
         return grade;
     }
 
+    //the method that deals with the grading of the numbers within 0 and 100
+    public void setGrade(int grade){
+        if (grade>=0&&grade<=100){
+            this.grade=grade;
+        }
+        else{
+            this.grade=0;
+        }
+    }
+
+    //method that will bbe used to display student details
+    public void displayStudent(){
+        System.out.println("Name of Student:"+ name);
+        System.out.println("Student Grade:" +grade);
+
+    }
 
 }
