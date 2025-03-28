ae0dcedb54e904ed29f669a8df6a4e40cb51bbd2
NginaKamau <loisek.ngina@strathmore.edu>
2025-03-28 20:09:35 +0300

    Implementation of method overloading by creating two methods but with two different parameters

---
+++ MathOperations.java
@@ -0,0 +1,10 @@
+public class MathOperations {
+    //the method that allows two integers
+    public int multiply(int a,int b){
+        return a*b;
+    }
+    //the method that allows three integers
+    public int multiply(int a,int b,int c){
+        return a*b*c;
+    }
+}
