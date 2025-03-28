c952d2f1508772af6bbea9b8489142a18d7b29b5
NginaKamau <loisek.ngina@strathmore.edu>
2025-03-28 19:41:31 +0300

    Creation of the Main class to act as a test class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[]args){
+        Student student1=new Student("Ngina",85);
+        Student student2=new Student("Alvin",300);
+
+        student1.displayStudent();
+        student2.displayStudent();
+    }
+}
