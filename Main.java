edb7e3891e529a563496f7add82d04f7752c8341
NginaKamau <loisek.ngina@strathmore.edu>
2025-03-28 20:45:28 +0300

    Creating objects in the Main class to demonstrate abstraction

--- Main.java
+++ Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[]args){
+
+        //Demonstrates encapsulation
         Student student1=new Student("Ngina",85);
         Student student2=new Student("Alvin",300);
 
         student1.displayStudent();
         student2.displayStudent();
 
+        //Demonstrates polymorphism
         MathOperations math = new MathOperations();
         System.out.println("Multiplication of 2 numbers: " + math.multiply(2, 3));
         System.out.println("Multiplication of 3 numbers: " + math.multiply(2, 3, 7));
@@ -16,7 +19,18 @@ public class Main {
 
         myDog.makeSound();
         myCat.makeSound();
+
+        //Demonstrates abstraction
+        Appliance myFan = new Fan();
+        Appliance myTV = new TV();
+
+        myFan.turnOn();
+        myTV.turnOn();
     }
+
+
+
+
 
 
 
