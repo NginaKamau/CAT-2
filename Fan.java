fba83f915d2637f60f9a458028a76f2a0b583ef4
NginaKamau <loisek.ngina@strathmore.edu>
2025-03-28 20:37:47 +0300

    Creation of subclasses Fan and TV from the abstract class Appliance which gives the body of the method since it cant be given in the abstract class

---
+++ Fan.java
@@ -0,0 +1,8 @@
+public class Fan extends Appliance {
+    @Override
+    public void turnOn(){
+        System.out.println("Upon turning on,the fan is currently spinning");
+
+    }
+
+}
---
+++ TV.java
@@ -0,0 +1,6 @@
+public class TV extends Appliance{
+    @Override
+    public void turnOn(){
+        System.out.println("Upon turning on.the Tv is now broadcasting and showing various channels");
+    }
+}
